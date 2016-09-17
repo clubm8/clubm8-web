416e10791bf4c270cf06ac425dd7ea142c5849d7
Philipp Christen <philipp.christen@gmail.com>
2016-09-17 13:04:59 +0200

    simple template change

--- views.py
+++ views.py
@@ -3,5 +3,7 @@ from django.views import generic
 
 from clubm8core import models
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
     template_name = 'clubm8web/index.html'
+
+    model = models.Slot
